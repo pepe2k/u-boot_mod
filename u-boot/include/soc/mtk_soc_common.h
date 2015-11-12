2bc1b721807fabccea0dbd89a2d672161a44b095
Piotr Dymacz <pepe2k@gmail.com>
2015-11-12 22:56:23 +0100

    Introduce new header files for SoC register defines
    As it turned out, most of QC/A WiSoC are very similar
    from register structures and addresses point of view.
    
    What's more, it seems that even when some registers
    or fields are slightly different in name, they still
    mean the same and work in exactly same way.
    
    So, instead of using approach from old Atheros SDK,
    where we have hundreds of different header files,
    for different chips, we will use only one. And in
    case of different address for the same register or
    different fields size/position, etc., we will use
    preprocessor and SOC_TYPE value.
    
    Add also MediaTek/Ralink headers file, we are going
    to fill it soon.

---
+++ u-boot/include/soc/mtk_soc_common.h
@@ -0,0 +1,14 @@
+/*
+ * MediaTek/Ralink Wireless SOC common registers definitions
+ *
+ * Copyright (C) 2014 Piotr Dymacz <piotr@dymacz.pl>
+ *
+ * SPDX-License-Identifier:GPL-2.0
+ */
+
+#ifndef _MTK_SOC_COMMON_H_
+#define _MTK_SOC_COMMON_H_
+
+#include <soc/soc_common.h>
+
+#endif /* _MTK_SOC_COMMON_H_ */
