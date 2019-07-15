21d1b97d156d00b75e36a0fbfceac61d218f582f
Tomislav Požega <pozega.tomislav@gmail.com>
2019-07-15 22:15:38 +0200

    define default CPU/DDR PLL dither values for recovery too
    If we want to use default device clocks in recovery mode,
    the safe list will get bigger with each supported chip so
    add default (zero) dither defines.
    
    Signed-off-by: Tomislav Požega <pozega.tomislav@gmail.com>

--- u-boot/include/soc/qca95xx_pll_init.h
+++ u-boot/include/soc/qca95xx_pll_init.h
@@ -1505,28 +1505,21 @@
 	#define QCA_PLL_CPU_PLL_CFG_REG_VAL_SAFE_XTAL25		_qca95xx_cpu_pll_cfg_reg_val(22, 1, 1, 0, 0)
 	#define QCA_PLL_DDR_PLL_CFG_REG_VAL_SAFE_XTAL25		_qca95xx_ddr_pll_cfg_reg_val(16, 1, 1, 0, 0)
 	#define QCA_PLL_CPU_DDR_CLK_CTRL_REG_VAL_SAFE_XTAL25	_qca95xx_cpu_ddr_clk_ctrl_reg_val(1, 1, 2, 1, 1, 1)
-	#define QCA_PLL_CPU_PLL_DITHER_REG_VAL_SAFE_XTAL25	_qca95xx_cpu_pll_dither_reg_val(0)
-	#define QCA_PLL_DDR_PLL_DITHER_REG_VAL_SAFE_XTAL25	_qca95xx_ddr_pll_dither_reg_val(0)
 
 	#define QCA_PLL_CPU_PLL_CFG_REG_VAL_SAFE_XTAL40		_qca95xx_cpu_pll_cfg_reg_val(13, 1, 1, 0, 0)
 	#define QCA_PLL_DDR_PLL_CFG_REG_VAL_SAFE_XTAL40		_qca95xx_ddr_pll_cfg_reg_val(10, 1, 1, 0, 0)
 	#define QCA_PLL_CPU_DDR_CLK_CTRL_REG_VAL_SAFE_XTAL40	_qca95xx_cpu_ddr_clk_ctrl_reg_val(1, 1, 2, 1, 1, 1)
 	#define QCA_PLL_CPU_PLL_DITHER_REG_VAL_SAFE_XTAL40	_qca95xx_cpu_pll_dither_reg_val(48)
-	#define QCA_PLL_DDR_PLL_DITHER_REG_VAL_SAFE_XTAL40	_qca95xx_ddr_pll_dither_reg_val(0)
 
 	#define QCA_SPI_CTRL_REG_VAL_SAFE			_qca95xx_spi_ctrl_addr_reg_val(8, 1, 0, 2)
 #else
 	#define QCA_PLL_CPU_PLL_CFG_REG_VAL_SAFE_XTAL25		_qca95xx_cpu_pll_cfg_reg_val(16, 1, 1, 0, 0)
 	#define QCA_PLL_DDR_PLL_CFG_REG_VAL_SAFE_XTAL25		_qca95xx_ddr_pll_cfg_reg_val(16, 1, 1, 0, 0)
 	#define QCA_PLL_CPU_DDR_CLK_CTRL_REG_VAL_SAFE_XTAL25	_qca95xx_cpu_ddr_clk_ctrl_reg_val(1, 1, 2, 1, 1, 1)
-	#define QCA_PLL_CPU_PLL_DITHER_REG_VAL_SAFE_XTAL25	_qca95xx_cpu_pll_dither_reg_val(0)
-	#define QCA_PLL_DDR_PLL_DITHER_REG_VAL_SAFE_XTAL25	_qca95xx_ddr_pll_dither_reg_val(0)
 
 	#define QCA_PLL_CPU_PLL_CFG_REG_VAL_SAFE_XTAL40		_qca95xx_cpu_pll_cfg_reg_val(10, 1, 1, 0, 0)
 	#define QCA_PLL_DDR_PLL_CFG_REG_VAL_SAFE_XTAL40		_qca95xx_ddr_pll_cfg_reg_val(10, 1, 1, 0, 0)
 	#define QCA_PLL_CPU_DDR_CLK_CTRL_REG_VAL_SAFE_XTAL40	_qca95xx_cpu_ddr_clk_ctrl_reg_val(1, 1, 2, 1, 1, 1)
-	#define QCA_PLL_CPU_PLL_DITHER_REG_VAL_SAFE_XTAL40	_qca95xx_cpu_pll_dither_reg_val(0)
-	#define QCA_PLL_DDR_PLL_DITHER_REG_VAL_SAFE_XTAL40	_qca95xx_ddr_pll_dither_reg_val(0)
 
 	#define QCA_SPI_CTRL_REG_VAL_SAFE			_qca95xx_spi_ctrl_addr_reg_val(10, 1, 0, 2)
 #endif
@@ -1554,6 +1547,14 @@
 	#define QCA_PLL_CPU_PLL_DITHER_REG_VAL_XTAL40	_qca95xx_cpu_pll_dither_reg_val(0)
 #endif
 
+#ifndef QCA_PLL_CPU_PLL_DITHER_REG_VAL_SAFE_XTAL25
+	#define QCA_PLL_CPU_PLL_DITHER_REG_VAL_SAFE_XTAL25	_qca95xx_cpu_pll_dither_reg_val(0)
+#endif
+
+#ifndef QCA_PLL_CPU_PLL_DITHER_REG_VAL_SAFE_XTAL40
+	#define QCA_PLL_CPU_PLL_DITHER_REG_VAL_SAFE_XTAL40	_qca95xx_cpu_pll_dither_reg_val(0)
+#endif
+
 /* DDR PLL dither register values */
 #ifndef QCA_PLL_DDR_PLL_DITHER_REG_VAL_XTAL25
 	#define QCA_PLL_DDR_PLL_DITHER_REG_VAL_XTAL25	_qca95xx_ddr_pll_dither_reg_val(0)
@@ -1561,6 +1562,14 @@
 
 #ifndef QCA_PLL_DDR_PLL_DITHER_REG_VAL_XTAL40
 	#define QCA_PLL_DDR_PLL_DITHER_REG_VAL_XTAL40	_qca95xx_ddr_pll_dither_reg_val(0)
+#endif
+
+#ifndef QCA_PLL_DDR_PLL_DITHER_REG_VAL_SAFE_XTAL25
+	#define QCA_PLL_DDR_PLL_DITHER_REG_VAL_SAFE_XTAL25	_qca95xx_ddr_pll_dither_reg_val(0)
+#endif
+
+#ifndef QCA_PLL_DDR_PLL_DITHER_REG_VAL_SAFE_XTAL40
+	#define QCA_PLL_DDR_PLL_DITHER_REG_VAL_SAFE_XTAL40	_qca95xx_ddr_pll_dither_reg_val(0)
 #endif
 
 #endif /* _QCA95XX_PLL_INIT_H_ */
